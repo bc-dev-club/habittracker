c34d5b83732ea41bf3b15489f2092ab3d874e845
ponnkotu48 <kanatani.0208@gmail.com>
2025-03-06 22:25:19 +0900

    rootの背景色を白に変更と定期レポート一覧の表示

--- frontend/src/pages/reports.tsx
+++ frontend/src/pages/reports.tsx
@@ -1,7 +1,20 @@
+import { CSSProperties } from "react";
+
+const Styles: { [key: string]: CSSProperties } = {
+  reportsTitle: {
+    margin : "140px",
+    fontFamily : "'Noto Sans JP', sans-serif",
+    fontSize : "200%",
+    textAlign : "center",
+  }
+};
+
 export const Reports = () => {
     return (
       <div>
-        <h1>Test</h1>
+        <h1 style={Styles.reportsTitle}>定期レポート一覧</h1>
+        <ul>
+        </ul>
       </div>
     );
   };
