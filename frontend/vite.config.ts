11ee6d25c231dd0cca7461395beef7080e436946
Tai-Yaki <taiki.ymd17@gmail.com>
2025-04-06 16:31:45 +0900

    feat: パスエイリアスを設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      types: "/src/types",
+    },
+  },
   server: {
     watch: {
       usePolling: true,
